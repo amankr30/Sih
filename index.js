6e0e7c0c7a219ea46c757f11b43db90369d7741a
Aman Kumar <121495302+amankr30@users.noreply.github.com>
2023-09-26 10:43:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function to open the popup
+// function openPopup() {
+//     const popup = document.querySelector(".popup");
+//     popup.style.display = "block";
+// }
+
+// Function to close the popup
+function closePopup() {
+    const popup = document.querySelector(".popup");
+    popup.style.display = "none";
+}
+
+// Open the popup after a delay (e.g., 5 seconds)
+// setTimeout(openPopup, 5000);
